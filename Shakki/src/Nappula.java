35e7f4865333bf8c572f29ee4a05fdc0bbf347bf
Tatu <Tatu@Surinaattori>
2018-02-19 13:01:23 +0200

    Lisättiin compareTo() - metodi Comparablen toteutusta varten

--- Shakki/src/Nappula.java
+++ Shakki/src/Nappula.java
@@ -1,8 +1,9 @@
 
-public abstract class Nappula {
+public abstract class Nappula implements Comparable<Nappula>{
 	
 	public final Vari vari;
 	
+	//Sijainti: ensimmäinen arvo vastaa pystyrivin kirjainta a-h ja toinen vaakarivin numeroa
 	private int[] sijainti = new int[2];
 	
 	public Nappula(Vari vari, int[] sijainti){
@@ -29,6 +30,35 @@ public abstract class Nappula {
 			throw new IllegalArgumentException("Nappula ulkona laudalta!");
 		}
 		this.sijainti = sijainti;
+	}
+	
+	/*
+	 * Vertailee nappuloita sijainnin perusteella. Ensin vaakarivin perusteella
+	 * ylhäältä alaspäin ja sitten pystysarakkeen perusteella vasemmalta oikealle.
+	 * 
+	 * @param nappula
+	 */
+	
+	
+	@Override
+	public int compareTo(Nappula toinen){
+		
+		
+		//Jos ollaan ylemmällä vaakarivillä kuin toinen nappula
+		if(this.sijainti[1] > toinen.annaSijainti()[1]){
+			return -1;
+		}
+		//Jos ollaan alemmalla
+		else if(this.sijainti[1] < toinen.annaSijainti()[1]){
+			return 1;
+		}
+		
+		//Jos ollaan samalla rivillä katsotaan sarake
+		if(this.sijainti[0] < toinen.annaSijainti()[1]){
+			return -1;
+		}
+		return 1;
+		
 	}
 	
 	
