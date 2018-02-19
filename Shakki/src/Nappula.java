d300005b122c808a3c87637a5b2ced00b7d95c65
Tatu <Tatu@Surinaattori>
2018-02-19 12:36:02 +0200

    Lisättiin nappulaan muutujat ja metodit: Vari vari int[] sijainti konstruktori bool voikoLiikkuaRuutuun int[] annaSijainti void asetaSijainti

--- Shakki/src/Nappula.java
+++ Shakki/src/Nappula.java
@@ -1,4 +1,27 @@
 
 public abstract class Nappula {
-
+	
+	public final Vari vari;
+	
+	private int[] sijainti = new int[2];
+	
+	public Nappula(Vari vari, int[] sijainti){
+		this.vari = vari;
+		this.sijainti = sijainti;
+		
+	}
+	
+	
+	
+	public abstract boolean voikoLiikkuaRuutuun();
+	
+	public int[] annaSijainti(){
+		return sijainti;
+	}
+	
+	public void asetaSijainti(int[] sijainti){
+		this.sijainti = sijainti;
+	}
+	
+	
 }
