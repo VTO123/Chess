1a16f2c895a7f2f26b269e2a3bf06a86bcdaed41
teemu <teemu@130.232.225.83>
2018-02-20 15:32:56 +0200

    poistettu import java.util.Map

--- Shakki/src/Lauta.java
+++ Shakki/src/Lauta.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Map;
 
 public class Lauta implements Serializable {
 
